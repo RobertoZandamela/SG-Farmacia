896331a42ce88c5ef73f576d2b78934404431c46
Roberto Zandamela <robertozandamela@gmail.com>
2020-05-13 10:37:33 +0100

    Tela login Feita

--- src/sg/farmacia/cadastro/Controller/LoginController.java
+++ src/sg/farmacia/cadastro/Controller/LoginController.java
@@ -7,7 +7,11 @@ package sg.farmacia.cadastro.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -16,9 +20,23 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private AnchorPane label_nomeFarmacia;
+
+    @FXML
+    private ImageView img_logo;
+
+    @FXML
+    private Button bt_entrar;
+
+    @FXML
+    private Button bt_cancelar;
+
+    @FXML
+    private ImageView img_iconUsername;
+
+    @FXML
+    private ImageView img_iconSenha;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO

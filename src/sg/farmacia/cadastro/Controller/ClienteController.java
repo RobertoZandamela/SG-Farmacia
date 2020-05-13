87a39bb07e9e2b02393c4be31cbd8ca8fbfea709
Roberto Zandamela <robertozandamela@gmail.com>
2020-05-13 22:27:43 +0100

    Conclusão Tela cadastro de cliente e Fornecedor... Não há interações ainda. Sem acesso dos dados no controller e muito menos no model apenas telas feitas.

--- src/sg/farmacia/cadastro/Controller/ClienteController.java
+++ src/sg/farmacia/cadastro/Controller/ClienteController.java
@@ -7,7 +7,17 @@ package sg.farmacia.cadastro.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -16,9 +26,165 @@ import javafx.fxml.Initializable;
  */
 public class ClienteController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TitledPane panel_cadastroClente;
+
+    @FXML
+    private Button bt_novo;
+
+    @FXML
+    private Button bt_alterar;
+
+    @FXML
+    private Button bt_gravar;
+
+    @FXML
+    private Button bt_procurar;
+
+    @FXML
+    private Button bt_excluir;
+
+    @FXML
+    private Pane panel_formulario;
+
+    @FXML
+    private Label lb_codigo;
+
+    @FXML
+    private Label lb_dataCadastro;
+
+    @FXML
+    private Label lb_nomeCliente;
+
+    @FXML
+    private Label lb_contacto;
+
+    @FXML
+    private Label lb_contactoAlternativo;
+
+    @FXML
+    private TextField text_codigo;
+
+    @FXML
+    private TextField text_dataCadastro;
+
+    @FXML
+    private TextField text_nomeCliente;
+
+    @FXML
+    private TextField text_contacto;
+
+    @FXML
+    private TextField text_ContactoAlternativo;
+
+    @FXML
+    private Label lb_tipoDocumento;
+
+    @FXML
+    private Label lb_numDocumento;
+
+    @FXML
+    private Label lb_localEmissao;
+
+    @FXML
+    private Label lb_dataEmissao;
+
+    @FXML
+    private Label lb_fotoClliente;
+
+    @FXML
+    private ComboBox<?> txt_tipoDocumento;
+
+    @FXML
+    private TextField txt_numDocumento;
+
+    @FXML
+    private TextField txt_localEmissao;
+
+    @FXML
+    private TextField txt_DataEmissao;
+
+    @FXML
+    private Label lb_estadoCivil;
+
+    @FXML
+    private Label lb_genero;
+
+    @FXML
+    private Label lb_naturalidade;
+
+    @FXML
+    private Label lb_dataNascimento;
+
+    @FXML
+    private ComboBox<?> txt_estadoCivil;
+
+    @FXML
+    private ComboBox<?> txt_genero;
+
+    @FXML
+    private TextField txt_naturalidade;
+
+    @FXML
+    private TextField txt_dataNascimento;
+
+    @FXML
+    private Label lb_numCasa;
+
+    @FXML
+    private Label lb_bairro;
+
+    @FXML
+    private Label lb_distrito;
+
+    @FXML
+    private Label lb_provincia;
+
+    @FXML
+    private TextField txt_numCasa;
+
+    @FXML
+    private Label lb_quarteirao;
+
+    @FXML
+    private TextField txt_rua;
+
+    @FXML
+    private TextField txt_quarteirao;
+
+    @FXML
+    private Label lb_rua;
+
+    @FXML
+    private TextField txt_bairro;
+
+    @FXML
+    private TextField txt_distrito;
+
+    @FXML
+    private TextField txt_provincia;
+
+    @FXML
+    private ImageView img_fotoCliente;
+
+    @FXML
+    private Label lb_email;
+
+    @FXML
+    private TextField txt_email;
+
+    @FXML
+    private Button bt_inserirFotoCliente;
+
+    @FXML
+    private Label lb_observacao;
+
+    @FXML
+    private TextArea txt_observacao;
+    
+    @FXML
+    private SplitMenuButton tipoDocumento;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- src/sg/farmacia/cadastro/Controller/FornecedorController.java
+++ src/sg/farmacia/cadastro/Controller/FornecedorController.java
@@ -7,7 +7,13 @@ package sg.farmacia.cadastro.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -16,9 +22,83 @@ import javafx.fxml.Initializable;
  */
 public class FornecedorController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button bt_novo;
+
+    @FXML
+    private Button bt_alterar;
+
+    @FXML
+    private Button bt_gravar;
+
+    @FXML
+    private Button bt_procurar;
+
+    @FXML
+    private Button bt_excluir;
+
+    @FXML
+    private Label lb_Codigo;
+
+    @FXML
+    private Label lb_cadastro;
+
+    @FXML
+    private Label lb_nomeFornecedor;
+
+    @FXML
+    private Label lb_contacto;
+
+    @FXML
+    private Label lb_contactoAuxiliar;
+
+    @FXML
+    private TextField txt_codigo;
+
+    @FXML
+    private TextField txt_dataCadastro;
+
+    @FXML
+    private TextField txt_nomeFornecedor;
+
+    @FXML
+    private TextField txt_contacto;
+
+    @FXML
+    private TextField txt_contactoAuxiliar;
+
+    @FXML
+    private Label lb_email;
+
+    @FXML
+    private TextField txt_email;
+
+    @FXML
+    private ImageView img_lotipo;
+
+    @FXML
+    private Label lb_logotipoFornecedor;
+
+    @FXML
+    private Label lb_nuit;
+
+    @FXML
+    private TextField txt_nuit;
+
+    @FXML
+    private TextField txt_localizacao;
+
+    @FXML
+    private Label lb_localizacao;
+
+    @FXML
+    private Button bt_adicionarLogo;
+
+    @FXML
+    private Label lb_observacao;
+
+    @FXML
+    private TextArea txt_observacao;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
